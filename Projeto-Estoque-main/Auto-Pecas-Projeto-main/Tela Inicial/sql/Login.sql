09e036ac2e00d6677b4714c4fd678afa683ec6d0
GabrielG2101 <134340580+GabrielG2101@users.noreply.github.com>
2024-06-12 16:09:20 -0300

    Delete Projeto-Estoque-main/Auto-Pecas-Projeto-main/Tela Inicial directory

--- Projeto-Estoque-main/Auto-Pecas-Projeto-main/Tela Inicial/sql/Login.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS usuario (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    nome TEXT NOT NULL,
-    email TEXT NOT NULL UNIQUE,
-    datanascimento TEXT NOT NULL,
-    cpf TEXT NOT NULL,
-    senha TEXT NOT NULL
-);
